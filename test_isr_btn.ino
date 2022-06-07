e9017cfeba23444d1cc7180ea57fac96e3617d0c
Azarko <Mikhail.Azarko@softline.com>
2022-06-07 11:16:50 +0300

    first commit

---
+++ test_isr_btn.ino
@@ -0,0 +1,49 @@
+/*
+При публичном размещении кода ссылка на первоисточник обязательна.
+*/
+
+#define btn_long_push 1000   // Длительность долинного нажатия кнопки
+volatile uint8_t btn_state;
+#define btn1_pin 3
+
+
+//********************************
+void setup() 
+{
+  pinMode(btn1_pin,INPUT_PULLUP); // BUTTON
+
+
+  PCICR =  0b00000100; // PCICR |= (1<<PCIE1); Включить прерывание PCINT2
+  PCMSK2 = 0b00001000; // Разрешить прерывание для  D3
+  
+  Serial.begin(115200);
+}
+
+//****************************************
+void loop() 
+{
+  switch (btn_state)
+    {
+  case 1:   {
+            Serial.print("нажатие");
+            Serial.println(btn_state);
+            }
+   break;
+   
+  case 2:  {
+            Serial.print("Отжатие ");
+            Serial.println(btn_state);  
+           }
+    break;
+    
+
+    }
+  btn_state=0; //обнуляем статус энкодера
+}
+
+//****************************************
+ISR (PCINT2_vect) //Обработчик прерывания от пинов D3
+{
+  uint8_t btn_state = bitRead(PIND, 4); //считываем состояние кнопки
+
+ }
