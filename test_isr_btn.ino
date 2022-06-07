d83cc2b3f9e1730d4dea537e82beb99e896c312d
Mikhail Azarka <89917198+aiminkai@users.noreply.github.com>
2022-06-07 22:46:21 +0300

    Update test_isr_btn.ino
    add println

--- test_isr_btn.ino
+++ test_isr_btn.ino
@@ -3,9 +3,9 @@
 */
 
 #define btn_long_push 1000   // Длительность долинного нажатия кнопки
-volatile uint8_t btn_state;
-#define btn1_pin 3
-
+uint8_t btn_state;
+#define btn1_pin 5
+boolean flag;
 
 //********************************
 void setup() 
@@ -16,34 +16,49 @@ void setup()
   PCICR =  0b00000100; // PCICR |= (1<<PCIE1); Включить прерывание PCINT2
   PCMSK2 = 0b00001000; // Разрешить прерывание для  D3
   
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 //****************************************
 void loop() 
 {
-  switch (btn_state)
-    {
-  case 0:   {
-            Serial.print("нажатие");
-            Serial.println(btn_state);
-            }
-   break;
-   
-  case 1:  {
-            Serial.print("Отжатие ");
-            Serial.println(btn_state);  
-           }
-    break;
-    
+ // boolean btn_state = !digitalRead(btn1_pin);
+//  if (!flag && btn_state){
+//    flag=true;
+//    Serial.println("нажатие");
+//  }
+//if (flag && !btn_state){
+//    flag=false;
+//    Serial.println("отжатие");
+//  }
 
-    }
-  btn_state=0; //обнуляем статус энкодера
+
+  
+//  switch (btn_state)
+//    {
+//  case 0:   {
+//            Serial.print("нажатие ");
+//            Serial.println(btn_state);
+//            }
+//   break;
+//   
+//  case 1:  {
+//            Serial.print("Отжатие ");
+//            Serial.println(btn_state);  
+//           }
+//    break;
+//    
+//
+//    }
+ // btn_state=0; //обнуляем статус энкодера
 }
 
 //****************************************
 ISR (PCINT2_vect) //Обработчик прерывания от пинов D3
 {
-  uint8_t btn_state = bitRead(PIND, 4); //считываем состояние кнопки
+   btn_state = bitRead(PIND, 4); //считываем состояние кнопки
+  if (btn_state==0){
+     Serial.println("нажатие");
+  }
 
  }
